6faec2bf9fcf70c22f18442d7e52a4420d289138
Benjamin Kilpatrick <befk500@gmail.com>
2025-09-08 11:29:54 -0500

    Changed frontend to use environment variables to configure backend endpoints

--- tutorial-frontend-app/src/App.js
+++ tutorial-frontend-app/src/App.js
@@ -3,10 +3,15 @@ import { useState, useEffect } from "react";
 import './App.css';
 import  Page from './Page.js';
 
+const backend_host = process.env.BACKEND_HOST || "localhost";
+const backend_port = process.env.BACKEND_PORT || 3001;
+
 function App() {
   const [pages, setPages] = useState([]);
+
+  const pages_endpoint_url = `http://${backend_host}:${backend_port}/pages`;
   useEffect(() => {
-	  fetch('http://localhost:3001/pages').then(response => response.json()).then(json => {
+	  fetch(pages_endpoint_url).then(response => response.json()).then(json => {
 		  //console.log("Pages: ")
 		  //console.log(pages)
 		  //console.log(json)
