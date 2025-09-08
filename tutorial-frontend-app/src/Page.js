27cf191807279720f41fcb34d651954baa91dab1
Benjamin Kilpatrick <befk500@gmail.com>
2025-09-08 11:41:59 -0500

    Changed frontend Page.js toresolve warnings about unnecessary includes

--- tutorial-frontend-app/src/Page.js
+++ tutorial-frontend-app/src/Page.js
@@ -1,5 +1,5 @@
 //import logo from './logo.svg';
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import './App.css';
 import ReactMarkdown from "react-markdown";
 
